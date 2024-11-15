a4fd37535b0d6d8c895db52a601deea67f9177d5
Nirbhay Malhotra <nirbhaymalhotra2@gmail.com>
2024-11-15 11:52:28 -0500

    Adding Database and Classes.

---
+++ Products.DB/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.DB
+{
+    public class Product
+    {
+        [Key] 
+        public int Id { get; set; }
+        public String Description {  get; set; }
+        public double Quantity { get; set; } 
+    }
+}
